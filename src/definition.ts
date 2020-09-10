7fce11f587d7e5c45aa783d35d807051f2fde54a
Weilin Shi <934587911@qq.com>
2020-09-10 12:34:57 +0800

    use checkout v2 and setup definition

--- src/definition.ts
+++ src/definition.ts
@@ -18,3 +18,44 @@ export interface EngineConfig extends Record<string, any> {
 	formTop: number;
 	formMaximum: number;
 };
+
+export interface unitClass {
+	name: string;
+	animePath: string;
+	meleeAttack: number;
+	rangeAttack: number;
+	initiative: number;
+	iconIndex: number;
+	huge: boolean;
+	levelMelee: number;
+	levelRange: number;
+	levelInit: number;
+	levelHP: number;
+	levelSP: number;
+	hp: number;
+	meleeTarget: number;
+	rangeTarget: number;
+	unitCost: number;
+	levelCost: number;
+	unitSkill: string;
+	unitSkillLevel: number;
+	
+}
+
+export interface GameSetup extends Record<string, any> {
+	mapWidth: number;
+	mapHeight: number;
+	startYear: number;
+	startMonth: number;
+	applicationTitle: string;
+	yearPrefix: string;
+	battleBGM: string;
+	defaultBGM: string;
+	aiBGM: string;
+	tpLimit: number;
+	spLimit: number;
+	reformLimit: number;
+	randomName1: string[];
+	randomName2: string[];
+	randomeName3: string[];
+}
